20a6961c127bab0e3250699b0ec90aadf75d4fa2
Dmitrii Vyaznikov <118927953+DmitriVyaznikov@users.noreply.github.com>
2026-03-19 02:27:38 +0300

    feat(core): add fp/ barrel export

---
+++ packages/core/src/fp/index.ts
@@ -0,0 +1,8 @@
+export * as Result from './result.js';
+export * as Maybe from './maybe.js';
+export * as Parser from './parser.js';
+export * as Tuple from './tuple.js';
+export * as Monoid from './monoid.js';
+export { pipe, flow } from './pipe.js';
+export { curry } from './curry.js';
+export { Iter } from './iterable.js';
